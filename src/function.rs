df634a9cdef88b333f7d8b08360e00ac2cefae4a
Matthias Muenzner <matthias.muenzner@jkaref.com>
2019-04-21 13:25:31 +0200

    Improved error handling. Recognizes that encryption / decryption is impossible and react accordingly.

--- src/function.rs
+++ src/function.rs
@@ -1,9 +1,4 @@
-use std::{
-    error::Error,
-    path::Path,
-    io::prelude::*,
-    fs::File,
-};
+use std::{path::Path, io::prelude::*, fs::File};
 
 extern crate openssl;
 
@@ -11,46 +6,30 @@ use openssl::{
     rsa::{Rsa, Padding},
     pkey::{Public, Private},
 };
+use self::openssl::error::ErrorStack;
 
 fn _read_from_file(name: &str) -> Vec<u8> {
     let mut result: Vec<u8> = Vec::new();
 
     let path = Path::new(name);
 
-    let mut file: File = match File::open(&path) {
-        Err(why) => panic!(
-            "Cannot open file {} {}",
-            path.display(),
-            why.description()
-        ),
-        Ok(file) => file
-    };
+    let file = File::open(&path);
 
-    match file.read_to_end(&mut result) {
-        Err(why) =>
-            panic!(
-                "couldn't read key {}: {}",
-                path.display(),
-                why.description()
-            ),
-        Ok(result) => result
-    };
-
+    if !file.is_err() {
+        file.unwrap().read_to_end(&mut result).unwrap_or_else(|_| {
+            result.clear();
+            0
+        });
+    }
     result
 }
 
-fn _load_private_key(private_key: &[u8], passphrase: &[u8]) -> Rsa<Private> {
-    match Rsa::private_key_from_pem_passphrase(private_key, passphrase) {
-        Err(why) => panic!("Cannot decrypt private key. {}", why),
-        Ok(result) => result
-    }
+fn _load_private_key(private_key: &[u8], passphrase: &[u8]) -> Result<Rsa<Private>, ErrorStack> {
+    Rsa::private_key_from_pem_passphrase(private_key, passphrase)
 }
 
-fn _load_public_key(public_key: &[u8]) -> Rsa<Public> {
-    match Rsa::public_key_from_pem(public_key) {
-        Err(why) => panic!("cannot load public key: {}", why),
-        Ok(result) => result
-    }
+fn _load_public_key(public_key: &[u8]) -> Result<Rsa<Public>, ErrorStack> {
+    Rsa::public_key_from_pem(public_key)
 }
 
 fn _encrypt_impl(public_key: &Rsa<Public>,
@@ -58,15 +37,11 @@ fn _encrypt_impl(public_key: &Rsa<Public>,
                  mut ciphertext: &mut Vec<u8>) -> usize {
     ciphertext.resize(public_key.size() as usize, 0);
 
-    let size = match public_key.public_encrypt(
+    public_key.public_encrypt(
         plaintext,
         &mut ciphertext,
         Padding::PKCS1_OAEP,
-    ) {
-        Err(why) => panic!("Encrypting plaintext failed: {}", why),
-        Ok(size) => size
-    };
-    size
+    ).unwrap()
 }
 
 fn _decrypt_impl(private_key: &Rsa<Private>,
@@ -74,15 +49,11 @@ fn _decrypt_impl(private_key: &Rsa<Private>,
                  ciphertext: &Vec<u8>) -> usize
 {
     plaintext.resize(private_key.size() as usize, 0);
-    let size = match private_key.private_decrypt(
+    private_key.private_decrypt(
         ciphertext,
         plaintext,
         Padding::PKCS1_OAEP,
-    ) {
-        Err(why) => panic!("Decrypting ciphertext failed. {}", why),
-        Ok(size) => size
-    };
-    size
+    ).unwrap()
 }
 
 pub fn encrypt(keyfile: &str,
@@ -91,11 +62,16 @@ pub fn encrypt(keyfile: &str,
     let _key = _load_public_key(
         &_read_from_file(keyfile)
     );
-    let size: usize = _encrypt_impl(
-        &_key,
-        &plaintext,
-        ciphertext,
-    );
+
+    let mut size: usize = 0;
+
+    if _key.is_ok() {
+        size = _encrypt_impl(
+            &_key.unwrap(),
+            &plaintext,
+            ciphertext,
+        );
+    }
     size
 }
 
@@ -107,11 +83,16 @@ pub fn decrypt(keyfile: &str,
         &_read_from_file(keyfile),
         passphrase.as_bytes(),
     );
-    let size: usize = _decrypt_impl(
-        &_key,
-        plaintext,
-        &ciphertext,
-    );
-    plaintext.retain(|&i| i > 0);
+
+    let mut size: usize = 0;
+
+    if _key.is_ok() {
+        size = _decrypt_impl(
+            &_key.unwrap(),
+            plaintext,
+            &ciphertext,
+        );
+        plaintext.retain(|&i| i > 0);
+    }
     size
 }
--- src/tests/function_tests.rs
+++ src/tests/function_tests.rs
@@ -25,3 +25,43 @@ fn test_encrypt_decrypt_roundtrip() {
     assert!(plaintext_len > 0);
     assert_eq!(plaintext.as_bytes().to_vec(), decrypted_plaintext);
 }
+
+#[test]
+fn test_encrypt_decrypt_no_public_key() {
+    let public_keyfile: &str = "src/tests/fixtures/no.such.public.key";
+
+    let plaintext: String = String::from("Test TXT");
+    let mut ciphertext: Vec<u8> = Vec::new();
+
+    let size: usize = function::encrypt(
+        public_keyfile,
+        plaintext.as_bytes().to_vec(),
+        &mut ciphertext,
+    );
+    assert_eq!(size, 0);
+}
+
+#[test]
+fn test_encrypt_decrypt_no_private_key() {
+    let private_keyfile: &str = "src/tests/fixtures/no.such.private.key";
+    let public_keyfile: &str = "src/tests/fixtures/sample.pub.key";
+
+    let plaintext: String = String::from("Test TXT");
+    let mut ciphertext: Vec<u8> = Vec::new();
+
+    let ciphertext_len: usize = function::encrypt(
+        public_keyfile,
+        plaintext.as_bytes().to_vec(),
+        &mut ciphertext,
+    );
+    assert!(ciphertext_len > 0);
+
+    let mut decrypted_plaintext: Vec<u8> = Vec::new();
+    let plaintext_len: usize = function::decrypt(
+        private_keyfile,
+        "",
+        &mut decrypted_plaintext,
+        ciphertext,
+    );
+    assert_eq!(plaintext_len, 0);
+}
