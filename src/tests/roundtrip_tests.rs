68268f45e0c101340d17b70247a30245c2b89537
Matthias Muenzner <matthias.muenzner@jkaref.com>
2019-05-05 20:19:43 +0200

    Provided generic abstraction for encrypter, decrypter.

--- src/tests/roundtrip_tests.rs
+++ src/tests/roundtrip_tests.rs
@@ -1,7 +1,9 @@
-
-use crate::decrypter;
-use crate::encrypter;
-use crate::function;
+#[cfg(test)]
+use crate::function::_read_from_file;
+#[cfg(test)]
+use crate::encrypter::{Encrypter, RsaEncrypter, Encryptable};
+#[cfg(test)]
+use crate::decrypter::{RsaDecrypter, Decryptable, Decrypter};
 
 #[test]
 fn test_encrypt_decrypt_roundtrip() {
@@ -11,15 +13,25 @@ fn test_encrypt_decrypt_roundtrip() {
     let plaintext: String = String::from("Test TXT");
     let mut ciphertext: Vec<u8> = Vec::new();
 
-    let public_key = function::_read_from_file(public_keyfile);
-    let private_key = function::_read_from_file(private_keyfile);
+    let public_key = _read_from_file(public_keyfile);
+    let private_key = _read_from_file(private_keyfile);
 
-    let ciphertext_len: usize = encrypter::RsaEncrypter::new(&public_key)
-        .encrypt(&plaintext.as_bytes().to_vec(), &mut ciphertext);
+    let rsa_encrypter: Encrypter<RsaEncrypter> = Encrypter::new(&public_key);
+
+    let ciphertext_len: usize = rsa_encrypter
+        .encrypt(
+            &plaintext.as_bytes().to_vec(),
+            &mut ciphertext
+        );
     assert!(ciphertext_len > 0);
 
     let mut decrypted_plaintext: Vec<u8> = Vec::new();
-    let plaintext_len: usize = decrypter::RsaDecrypter::new(&private_key, &Vec::new())
+
+    let rsa_decrypter: Decrypter<RsaDecrypter> = Decrypter::new(
+        &private_key, &Vec::new()
+    );
+
+    let plaintext_len: usize = rsa_decrypter
         .decrypt(&mut decrypted_plaintext, &ciphertext);
     assert!(plaintext_len > 0);
     assert_eq!(plaintext.as_bytes().to_vec(), decrypted_plaintext);
